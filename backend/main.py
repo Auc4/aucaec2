b97d4e0ee7fabe5f4accd92404b441027d6a897a
Auc4 <sebrafa14@gmail.com>
2025-10-01 19:28:57 -0500

    CORS en main.py

--- backend/main.py
+++ backend/main.py
@@ -3,4 +3,13 @@ from db import create_all_tables
 from routes import tasks
 
 app = FastAPI(lifespan=create_all_tables)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],      # acepta todos los orígenes
+    allow_credentials=True,
+    allow_methods=["*"],      # acepta todos los métodos (GET, POST, PUT, DELETE, etc)
+    allow_headers=["*"],      # acepta todos los headers
+)
+
 app.include_router(tasks)
